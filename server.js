d9170ee065e2d3cccacbbe4681fdd06ea02eb0f7
shani kupiec <shani.kupiec@gmail.com>
2022-06-21 17:21:23 +0300

    last changes

--- server.js
+++ server.js
@@ -3,34 +3,36 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+let historyList = [];
+
+app.get("/historyList", (req, res) => {
+  res.json(historyList);
+});
+
+app.delete("/historyList", (req, res) => {
+  historyList = []
+  res.send('done');
+});
+
 app.get("/nameInfo/:name", async (req, res) => {
   const name = req.params.name;
-  // const data = await axios.get(`https://api.genderize.io/?name=${name}`).then((res) => res.data);
-  // const countries = await axios.get(`https://api.nationalize.io/?name=${name}`).then((res) => res.data.country);
-  // data.countries = countries;
-  const data = {
-    name: "shani",
-    gender: "male",
-    probability: 0.63,
-    count: 1254,
-    countries: [
-      {
-        country_id: "PK",
-        probability: 0.5582731025528594,
-      },
-      {
-        country_id: "IL",
-        probability: 0.31766114029808673,
-      },
-      {
-        country_id: "ZA",
-        probability: 0.0329416287614195,
-      },
-    ],
-  };
+  const data = await axios.get(`https://api.genderize.io/?name=${name}`).then((res) => res.data);
+  const countries = await axios.get(`https://api.nationalize.io/?name=${name}`).then((res) => res.data.country);
+  data.countries = countries;
+  data.id = _makeId();
+  historyList.unshift(data);
   res.json(data);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+function _makeId(length = 5) {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
